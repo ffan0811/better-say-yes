cfa06ca7f94795d5ddafcdc130238dda05517a53
MJ <ffan0811@gmail.com>
2024-06-16 19:08:11 +0900

    ADD: font

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,13 @@ export default async function Index() {
     <Layout>
       <main className="flex-1 flex flex-col">
         <section className="h-screen absolute w-full left-0 top-0 flex justify-center items-center -z-[1]">
-          <div className="flex items-center text-7xl font-bold">
-            100%{" "}
-            <div className="mx-4">
+          <div className="flex items-center text-7xl 2xl:text-9xl font-bold space-x-12">
+            <span>100%</span>
+            <div className="">
               <ImageRotator />
-            </div>{" "}
-            100% fun
+            </div>
+            <span>100%</span>
+            <span>fun</span>
           </div>
         </section>
         <section></section>
