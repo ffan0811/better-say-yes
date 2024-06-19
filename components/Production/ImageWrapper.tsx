0adbd91045a505afa18df88c99012b692c0a6f11
MJ <ffan0811@gmail.com>
2024-06-19 21:59:30 +0900

    FIX: build error

--- components/Production/ImageWrapper.tsx
+++ components/Production/ImageWrapper.tsx
@@ -3,17 +3,20 @@
 import { ReactNode, useEffect } from "react";
 import { useImages } from "../image-provider";
 import { getImageUrls } from "@/actions/content";
+import { ImageProps } from "@/types/image";
 
 export default function ImageWrapper({
   contentId,
+  images,
   children,
 }: {
-  contentId: string;
+  contentId?: string;
+  images?: ImageProps[];
   children: ReactNode;
 }) {
   const { setViewableImages } = useImages();
 
-  const fetch = async () => {
+  const fetchImages = async () => {
     const { result, error } = await getImageUrls({
       contentId: contentId,
     });
@@ -23,7 +26,14 @@ export default function ImageWrapper({
   };
 
   useEffect(() => {
-    fetch();
+    if (!contentId) return;
+    fetchImages();
   }, [contentId]);
+
+  useEffect(() => {
+    if (images && images.length > 0) {
+      setViewableImages(images);
+    }
+  }, [images]);
   return <>{children}</>;
 }
