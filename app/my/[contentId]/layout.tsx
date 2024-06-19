5dd7bfe08acfe8b2c4a46ce6081de0484bcb7e69
MJ <ffan0811@gmail.com>
2024-06-19 23:08:38 +0900

    TEST: edge functions - fetch images

--- app/my/[contentId]/layout.tsx
+++ app/my/[contentId]/layout.tsx
@@ -1,7 +1,9 @@
-import { getImageUrls } from "@/actions/content";
 import ImageWrapper from "@/components/Production/ImageWrapper";
 import { ImageProvider } from "@/components/image-provider";
 import LoaderEntirePage from "@/components/loaderEntirePage";
+import { createClient } from "@/lib/supabase/server";
+import getBase64ImageUrl from "@/lib/utils/generateBlurPlaceholder";
+import { ImageProps } from "@/types/image";
 import { ReactNode, Suspense } from "react";
 
 export default async function ContentPageLayout({
@@ -15,17 +17,37 @@ export default async function ContentPageLayout({
     return <p>Incorrect URL</p>;
   }
 
-  const { result, error } = await getImageUrls({
-    contentId: params.contentId,
+  const supabase = createClient();
+
+  const { data, error } = await supabase.functions.invoke("fetch-images", {
+    body: { contentId: params.contentId, tableName: "contents" },
   });
 
   if (error) {
     return <p>{`Failed to fetch template images: ${JSON.stringify(error)}`}</p>;
   }
+
+  let reducedResults: ImageProps[] = [];
+
+  const blurImagePromises = data.data.map((image: { name: string }) => {
+    return getBase64ImageUrl({
+      imageName: image.name,
+      storageUrl: `/contents/${params.contentId}`,
+    });
+  });
+  const imagesWithBlurDataUrls = await Promise.all(blurImagePromises);
+
+  for (let i = 0; i < data.data.length; i++) {
+    reducedResults.push({
+      src: data.data[i].name,
+      blurDataUrl: imagesWithBlurDataUrls[i],
+    });
+  }
+
   return (
     <Suspense fallback={<LoaderEntirePage />}>
       <ImageProvider contentId={params.contentId}>
-        <ImageWrapper images={result}>{children}</ImageWrapper>
+        <ImageWrapper images={reducedResults}>{children}</ImageWrapper>
       </ImageProvider>
     </Suspense>
   );
