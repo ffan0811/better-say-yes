72f411c4cd02c82d4b00b4cdda7ff71a4bb3d703
MJ <ffan0811@gmail.com>
2024-06-19 22:25:08 +0900

    Revert "FIX: build error"
    This reverts commit 0adbd91045a505afa18df88c99012b692c0a6f11.

--- components/Production/ImageWrapper.tsx
+++ components/Production/ImageWrapper.tsx
@@ -3,20 +3,17 @@
 import { ReactNode, useEffect } from "react";
 import { useImages } from "../image-provider";
 import { getImageUrls } from "@/actions/content";
-import { ImageProps } from "@/types/image";
 
 export default function ImageWrapper({
   contentId,
-  images,
   children,
 }: {
-  contentId?: string;
-  images?: ImageProps[];
+  contentId: string;
   children: ReactNode;
 }) {
   const { setViewableImages } = useImages();
 
-  const fetchImages = async () => {
+  const fetch = async () => {
     const { result, error } = await getImageUrls({
       contentId: contentId,
     });
@@ -26,14 +23,7 @@ export default function ImageWrapper({
   };
 
   useEffect(() => {
-    if (!contentId) return;
-    fetchImages();
+    fetch();
   }, [contentId]);
-
-  useEffect(() => {
-    if (images && images.length > 0) {
-      setViewableImages(images);
-    }
-  }, [images]);
   return <>{children}</>;
 }
