ba2328987ad71a4aba331dfb6abfc72540fae48c
MJ <ffan0811@gmail.com>
2024-06-27 15:32:37 +0900

    UPADTE: layout

--- components/Production/MainContents.tsx
+++ components/Production/MainContents.tsx
@@ -2,7 +2,9 @@ import { ReactNode } from "react";
 import NoButton from "./NoButton";
 
 export const QUESTION_COMMON_CLASSES =
-  "leading-9 text-center break-words text-3xl whitespace-pre-line";
+  "leading-9 text-center break-words text-xl md:text-3xl whitespace-pre-line";
+
+export const ANSWER_BUTTON_COMMON_CLASSES = "min-w-32 md:min-w-40";
 
 type MainContentsProps = {
   title: ReactNode;
@@ -18,7 +20,7 @@ export default function MainContents({
   return (
     <>
       {title}
-      <div className="flex space-x-4 justify-center mt-8">
+      <div className="flex space-x-4 justify-center mt-4 md:mt-8">
         <NoButton themeColor={themeColor} />
         {children}
       </div>
