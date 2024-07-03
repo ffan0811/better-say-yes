007d6740abc5364daec467947036b0b1db07c180
MJ <ffan0811@gmail.com>
2024-07-03 21:34:45 +0900

    UPDATE: styles

--- components/Production/AfterYesContents.tsx
+++ components/Production/AfterYesContents.tsx
@@ -66,6 +66,7 @@ export default function AfterYesContents({
               src={src}
               width={720}
               height={480}
+              quality={100}
               sizes="(max-width: 640px) 100vw,
                 (max-width: 1280px) 50vw,
                 (max-width: 1536px) 33vw,
